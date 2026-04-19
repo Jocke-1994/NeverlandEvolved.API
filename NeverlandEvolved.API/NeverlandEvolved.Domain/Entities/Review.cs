5a2ad378673620aba5c61f35a2fb0315679b32d7
Joakim Rehn <jocke.94@hotmail.se>
2026-04-19 18:34:15 +0200

    Add domain entities

---
+++ NeverlandEvolved.API/NeverlandEvolved.Domain/Entities/Review.cs
@@ -0,0 +1,17 @@
+﻿namespace NeverlandEvolved.Domain.Entities
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public int Rating { get; set; } // T.ex. 1-5
+        public string Comment { get; set; } = string.Empty;
+
+        // Koppling till Game
+        public int GameId { get; set; }
+        public Game Game { get; set; } = null!;
+
+        // Koppling till User
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+    }
+}
---
+++ NeverlandEvolved.API/NeverlandEvolved.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace NeverlandEvolved.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        // En användare kan ha många recensioner (1-till-många)
+        public ICollection<Review> Reviews { get; set; } = new List<Review>();
+    }
+}

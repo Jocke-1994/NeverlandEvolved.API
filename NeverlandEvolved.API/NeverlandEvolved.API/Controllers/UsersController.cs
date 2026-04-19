d3701d14c53c9d8119073febfe70d06bf1345328
Joakim Rehn <jocke.94@hotmail.se>
2026-04-19 19:57:34 +0200

    Add Users and Reviews controllers to prove relations

---
+++ NeverlandEvolved.API/NeverlandEvolved.API/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NeverlandEvolved.Domain.Entities;
+using NeverlandEvolved.Infrastructure.Data;
+
+namespace NeverlandEvolved.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // POST: api/Users (Skapar en användare)
+        [HttpPost]
+        public async Task<ActionResult<User>> CreateUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+    }
+}
